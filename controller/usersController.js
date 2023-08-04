da28440d4a4cf37356adec1f3d3ba34d7d9c0a23
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-08-03 23:44:08 -0700

    JWT  Token authorization added

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,4 +1,9 @@
 const knex = require("knex")(require("../knexfile"));
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+
 
 
 // Get users Details
@@ -79,37 +84,17 @@ function updateUsers(req, res) {
       res.status(500).json({ error: 'Failed to update user profile' });
     });
 }
-// // Function to update the user's balance in the database
-// function updateBalance(req, res) {
-//   console.log("request", req.body);
-//   const { balance } = req.body;
-//   const userId = req.params.userId;
-
-//   knex('users')
-//     .where('id', userId)
-//     .increment('balance', balance)
-//     .then(() => {
-//       console.log('Balance updated successfully');
-//       res.status(200).json({ message: 'Balance updated successfully' });
-//     })
-//     .catch((error) => {
-//       console.error('Failed to update balance:', error);
-//       res.status(500).json({ message: 'Failed to update balance' });
-//     });
-// };
-
-
 
 
 function updateBalance(req, res) {
-  // const { userId } = req.params;
+  const  userId  = req.params;
   const { balance } = req.body;
-console.log("balance",balance);
+// console.log("balance",balance);
 
-  // console.log("userId", userId);
+//   console.log("userId", userId);
 
   knex("users")
-    .where("id", 1)
+    .where("id", userId)
     .update({ balance })
     .then(() => {
       res.sendStatus(200);
@@ -121,8 +106,119 @@ console.log("balance",balance);
     });
 }
 
+
+// Add new user when someone signup
+function newUsers(req, res) {
+  const {
+    first_name,
+    last_name,
+    street_number,
+    city,
+    province,
+    country,
+    phone_number,
+    email,
+    username,
+    password,
+    confirm_password,
+  } = req.body;
+  console.log("pass",password)
+  
+  // Check if the passwords match before proceeding
+  if (password !== confirm_password) {
+    return res.status(400).json({ error: 'Passwords do not match.' });
+    
+  }
+
+  // Generate a salt with a cost of 10
+  const saltRounds = 10;
+
+  // Hash the password with the generated salt
+  bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
+    console.log("confffirm",confirm_password )
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to update user profile' });
+    }
+
+    const newUser = {
+      first_name,
+      last_name,
+      phone_number,
+      email,
+      username,
+      password: hashedPassword, // Save the hashed password to the database
+      confirm_password: hashedPassword,
+      street_number,
+      city,
+      province,
+      country,
+      img:'',
+      balance:'',
+      last_opened:'',
+      last_payment:'',
+      last_payment_date:'2023-06-11',
+      last_month_expense:'',
+    };
+
+    knex("users")
+      .insert(newUser)
+      .then(() => {
+        res.status(201).json({ message: 'User created successfully', user: newUser });
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to create user' });
+      });
+  });
+}
+
+function generateAccessToken(user) {
+  // Use jwt.sign() to generate the access token
+  const accessToken = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '10s' });
+  return accessToken;
+}
+
+function userLogin (req,res) {
+const { username, password} = req.body;
+
+knex('users')
+.where('username',username)
+.first()
+.then((user)=>{
+  if(!user){
+    return res.status(401).json(
+      {error:'Invalid username or password' });
+    
+  }
+ // Compare the provided password with the hashed password in the database
+ bcrypt.compare(password, user.password, (err, isPasswordMatch) => {
+  if (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Failed to login' });
+  }
+
+  if (!isPasswordMatch) {
+    return res.status(401).json({ error: 'Invalid username or password' });
+  }
+
+  const accessToken = generateAccessToken({ username: user.username, userId: user.id });
+
+  // Return the token to the frontend
+  res.json({ userId: user.id, accessToken });
+});
+})
+.catch((error) => {
+console.error('Error during login:', error);
+res.status(500).json({ error: 'Failed to login' });
+});
+}
+
+
 module.exports = {
-  updateUsers,
-  getSingleUser,
-  updateBalance
+updateUsers,
+getSingleUser,
+updateBalance,
+newUsers,
+userLogin,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,58 @@
 const router = require("express").Router();
 const usersController = require("../controller/usersController");
+require('dotenv').config();
+
+
 
 const {
   getSingleUser,
   updateUsers,
-  updateBalance
+  updateBalance,
+  newUsers,
+  userLogin,
+ 
 } = usersController;
+
+// Middleware to verify JWT token
+const authenticateToken = (req, res, next) => {
+  console.log("req",req)
+  const token = req.header('Authorization')?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'Access denied. No token provided.' });
+  console.log("token",token)
+  
+  jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+    if (err) return res.status(403).json({ error: 'Invalid token.' });
+    console.log("decoded",decodedToken.userId)
+    try {
+      const userId = decodedToken.userId;
+      const user = await getUserById(userId);
+      
+
+      if (!user) {
+        return res.status(404).json({ error: `User ID ${userId} not found.` });
+      }
+
+      req.user = user;
+      
+      
+      next();
+    } catch (error) {
+      console.error(`Error fetching user data for the token: ${error}`);
+      return res.status(500).json({ error: 'Failed to fetch userss data.' });
+    }
+  });
+};
 
 router.get("/:id", getSingleUser);
 router.put("/:id", updateUsers);
 router.put("/:id/updatebalance", updateBalance);
+router.post("/new",newUsers);
+router.post("/login", userLogin);
+router.get("/me", authenticateToken, (req, res) => {
+  console.log("req",req)
+  const userData = req.user 
+  
+  res.json(userData);
+});
 
 module.exports = router;
